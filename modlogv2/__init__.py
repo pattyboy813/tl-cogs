3290a7477e728abea82a2aa82ca58fe963c0e9cd
pattyboy813 <86346146+pattyboy813@users.noreply.github.com>
2025-09-23 15:30:36 +1000

    Update __init__.py

--- modlogv2/__init__.py
+++ modlogv2/__init__.py
@@ -1,7 +1,5 @@
-from .modlogv2 import ModLogV2
+from .modlogv2 import ModLogX
 
 async def setup(bot):
-    # Refuse to collide with any existing 'modlog' command
-    if bot.get_command("modlog"):
-        raise RuntimeError("ModLogV2: 'modlog' already exists (likely another cog). Unload it first.")
-    await bot.add_cog(ModLogV2(bot))
+    await bot.add_cog(ModLogX(bot))
+
