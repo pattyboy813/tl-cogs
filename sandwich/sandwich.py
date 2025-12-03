6f8dfaa7f7cef97ab9190b597b7a9bfe93dffe72
pattyboy813 <86346146+pattyboy813@users.noreply.github.com>
2025-12-03 11:35:25 +1100

    fixing loop

--- sandwich/sandwich.py
+++ sandwich/sandwich.py
@@ -5,8 +5,10 @@ import random
 import calendar
 
 import discord
-from redbot.core import commands, Config, tasks
+from redbot.core import commands, Config
+from redbot.core.tasks import loop
 from redbot.core.bot import Red
+
 
 
 # Duane's userID
@@ -33,7 +35,7 @@ class sandwich(commands.Cog):
         self.monthly_loop.cancel()
 
     # Background task runs once a day
-    @tasks.loop(hours=24)
+    @loop(hours=24)
     async def monthly_loop(self):
         today = datetime.datetime.utcnow()
         current_month = today.strftime("%Y-%m")
